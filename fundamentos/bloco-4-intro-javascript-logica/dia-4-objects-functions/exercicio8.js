c80d2159e6f7b1df2e0fae6cd3149f7ec38f8522
joaomattedi <joaommattedi@gmail.com>
2022-04-11 19:18:44 -0300

    exercicios 7 e 8 feitos

--- fundamentos/bloco-4-intro-javascript-logica/dia-4-objects-functions/exercicio8.js
+++ fundamentos/bloco-4-intro-javascript-logica/dia-4-objects-functions/exercicio8.js
@@ -11,12 +11,14 @@ let leitor = {
     ],
 };
   
-console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
 
-leitor.livrosFavoritos.push = {
+leitor.livrosFavoritos.push({
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
     autor: 'JK Rowling',
     editora: 'Rocco',
-  };
+  },
+)
+// console.log(leitor.livrosFavoritos);
 
-console.log(leitor.livrosFavoritos);
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
