5e3c3b6b05862717f04a0ee89f1942d9af86364c
joaomattedi <joaommattedi@gmail.com>
2022-04-20 13:42:07 -0300

    exercicio do dia finalizado com melhorias a serem feitas

--- fundamentos/bloco-5-js-DOM-events-webstorage/aula5.4/main.js
+++ fundamentos/bloco-5-js-DOM-events-webstorage/aula5.4/main.js
@@ -3,15 +3,18 @@ const textColor = document.querySelector('.prop-text-color');
 const fontSize = document.querySelector('.prop-font-size');
 const lineHeight = document.querySelector('.prop-line-heigth');
 const fontFamily = document.querySelector('.prop-font-family');
+const saveButton = document.querySelector('#save-visual');
+const restoreButton = document.querySelector('#restore-visual');
 
-bgColor.addEventListener('keydown',e => {
+
+bgColor.addEventListener('keydown', changeBgColor = e => {
     if(e.key === 'Enter'){
         document.body.style.backgroundColor = bgColor.value;
         bgColor.value = null;
     }
 });
 
-textColor.addEventListener('keydown', e => {
+textColor.addEventListener('keydown', changeTextColor = e => {
     if(e.key === 'Enter'){
         document.querySelector('main').style.color = textColor.value;
         document.querySelector('h1').style.color = textColor.value;
@@ -20,27 +23,64 @@ textColor.addEventListener('keydown', e => {
     }
 });
 
-fontSize.addEventListener('keydown', e =>{
+fontSize.addEventListener('keydown', changeFontSize = e =>{
     if(e.key === 'Enter'){
         document.querySelector('main').style.fontSize = fontSize.value + 'px';
         fontSize.value = null;
     }
 });
 
-lineHeight.addEventListener('keydown', e => {
+lineHeight.addEventListener('keydown', changeLineHeight = e => {
     if(e.key === 'Enter'){
         document.querySelector('main').style.lineHeight = lineHeight.value + 'px';
         lineHeight.value = null;
     }
-})
+});
 
-fontFamily.addEventListener('keydown', e => {
-    if(e.key === 'Enter'){
-        let paragraphs = document.querySelectorAll('p');
-
-        for(let i of paragraphs){
-        i.style.fontFamily = fontFamily.value;
-        }
-        fontFamily.value = null;
+fontFamily.addEventListener('keydown', changeFontFamily = e => {
+  if(e.key === 'Enter'){
+    let paragraphs = document.querySelectorAll('p');
+   
+    for(let i of paragraphs){
+    i.style.fontFamily = fontFamily.value;
     }
-})
+    fontFamily.value = null;
+  }
+});
+
+saveButton.addEventListener('click', e => {
+
+  localStorage.setItem('bgColor',document.body.style.backgroundColor);
+  localStorage.setItem('textColor',document.querySelector('main').style.color);
+  localStorage.setItem('fontSize',document.querySelector('main').style.fontSize);
+  localStorage.setItem('lineHeight',document.querySelector('main').style.lineHeight);
+  localStorage.setItem('fontFamily',document.querySelector('p').style.fontFamily); 
+  localStorage.setItem('lastView',true);   
+});
+
+restoreButton.addEventListener('click', e => {
+  localStorage.setItem('lastView',false);
+});
+
+// window.onload = e => {
+
+// }
+if(localStorage.lastView === 'true'){
+  document.body.style.backgroundColor = localStorage.bgColor;
+  document.querySelector('main').style.color = localStorage.textColor;
+  document.querySelector('h1').style.color = localStorage.textColor;
+  document.querySelector('section').style.color = localStorage.textColor;
+  document.querySelector('main').style.fontSize = localStorage.fontSize;
+  document.querySelector('main').style.lineHeight = localStorage.lineHeight;
+  
+  let paragraphs = document.querySelectorAll('p'); 
+  for(let i of paragraphs){
+    i.style.fontFamily = localStorage.fontFamily;
+  }  
+}else{
+  for(let key in localStorage){
+    localStorage.removeItem(key);
+  }
+  localStorage.setItem('lastView',false);
+}    
+
