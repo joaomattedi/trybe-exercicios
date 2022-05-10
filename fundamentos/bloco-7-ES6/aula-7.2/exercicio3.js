79e1c940b34080491de7d2d7903f847688aa44f0
joaomattedi <joaommattedi@gmail.com>
2022-05-10 13:33:38 -0300

    exercicio 1 e 2 parte 3

---
+++ fundamentos/bloco-7-ES6/aula-7.2/exercicio3.js
@@ -0,0 +1,40 @@
+// Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie uma função para mostrar o tamanho de um objeto.
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function turnChange( lesson, value ) {
+  const objeto = {
+    turno : value,
+  }
+  Object.assign(lesson,objeto);
+  console.log(lesson);
+}
+
+turnChange(lesson2,'noite');
+
+function returnKeys(object) {
+  console.log(Object.keys(object));
+}
+
+returnKeys(lesson1);
