f4ef128fc74291ebc1f4db22f278779dfb13fa70
joaomattedi <joaommattedi@gmail.com>
2022-05-10 17:54:16 -0300

    exercicio 1.4

--- fundamentos/bloco-7-ES6/aula-7.3/sum.test.js
+++ fundamentos/bloco-7-ES6/aula-7.3/sum.test.js
@@ -3,3 +3,15 @@ const testSum = require('./sum');
 test('testando função', () => {
   expect(testSum(5,4)).toBe(9);
 });
+
+test('0+0 é igual a 0', () => {
+  expect(testSum(0,0)).toBe(0);
+});
+
+test('envia erro quando parametro nao é numero', () => {
+  expect(() => {testSum(4,'5')}).toThrowError();
+});
+
+test('mensagem de erro é parameters must be numbers', () => {
+  expect(() => {testSum(4,'5')}).toThrowError(new Error('parameters must be numbers'));
+});
