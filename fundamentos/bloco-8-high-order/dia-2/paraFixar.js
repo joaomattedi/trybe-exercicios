c4f88c763b48465a53ac7c0d1161716ca3f1a395
joaomattedi <joaommattedi@gmail.com>
2022-05-16 16:25:10 -0300

    parafixar

---
+++ fundamentos/bloco-8-high-order/dia-2/paraFixar.js
@@ -0,0 +1,47 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Adicione seu código aqui
+
+emailListInData.forEach((item) => {
+  showEmailList(item);
+})
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (array) => {
+  // Adiciona seu código aqui
+  return array.find((number) => number % 3 === 0 && number % 5 === 0)
+}
+
+console.log(findDivisibleBy3And5(numbers));
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((item) => item.length === 5);
+}
+
+console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  return musicas.find((item) => item.id === id)
+}
+
+console.log(findMusic('31031685'))
