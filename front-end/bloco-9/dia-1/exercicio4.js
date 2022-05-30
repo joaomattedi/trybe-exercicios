bd99ab8d37c87f23078d8b65a99056fefb671a61
João Mattedi <joaommattedi@gmail.com>
2022-05-30 12:09:02 -0300

    exercicios até 6

---
+++ front-end/bloco-9/dia-1/exercicio4.js
@@ -0,0 +1,15 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  setTimeout(() => {
+    console.log(getMarsTemperature());
+  }, messageDelay());
+};
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
