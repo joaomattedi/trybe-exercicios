fb00d5241d50a2b8c07fea75e064facf9a9f2b9c
João Mattedi <joaommattedi@gmail.com>
2022-07-22 08:57:35 -0300

    fixando

---
+++ index.js
@@ -0,0 +1,31 @@
+const Redux = require('redux');
+
+const fazerLogin = (email) => ({
+  type: "LOGIN",
+  email});
+
+const ESTADO_INICIAL = {
+  login: false,
+  email: "",
+};
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+    default: // No switch, sempre precisamos ter um case default!
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+store.dispatch(fazerLogin("alguem@email.com"));
+
+console.log(store.getState());
+
+// { login: true, email: 'alguem@email.com' }
